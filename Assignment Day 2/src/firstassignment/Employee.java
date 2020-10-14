6490b06ebd3c217fba578ee015043661a6365fa7
Flash252 <72822680+Flash252@users.noreply.github.com>
2020-10-14 17:22:14 +0530

    Add files via upload

---
+++ Assignment Day 2/src/firstassignment/Employee.java
@@ -0,0 +1,21 @@
+package firstassignment;
+
+public class Employee 
+{
+
+	String name;
+	int age;
+	String city;
+	
+	void Display()
+	{
+		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		System.out.println("The Employee Name is "+name+".");
+		System.out.println("Age: "+age+" years old");
+		System.out.println("City: "+city);
+		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		
+		
+	}
+	
+}
---
+++ Assignment Day 2/src/firstassignment/Main.java
@@ -0,0 +1,23 @@
+package firstassignment;
+
+public class Main 
+{
+
+	public static void main(String[] args)
+	{
+		
+		Employee e1=new Employee();
+		e1.name="Hitesh Sakhare";
+		e1.age=22;
+		e1.city="Mumbai";
+		
+		Employee e2=new Employee();
+		e2.name="Meet Rawal";
+		e2.age=23;
+		e2.city="Kalyan";
+					
+		e1.Display();
+		e2.Display();
+	}
+
+}
